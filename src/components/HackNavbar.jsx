b8baba8e3d80bd301201bddfd9b69e80b80f71dc
gbascans <gbascansc@gmail.com>
2023-07-07 16:19:02 -0300

    Aumentando de tamaño la pagina

--- src/components/HackNavbar.jsx
+++ src/components/HackNavbar.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
-import "../css/HackNavbar.css"
+import "../css/HackNavbar.css";
 function HackNavbar() {
   return (
     <section className="navbar-contenedor">
-      <div className='navbar'>
-      <Link to="/" className="hackflix">HACKFLIX</Link>
-      <div>
-      <Link to="/paginacion/1" className="link">Ver por Paginas</Link>
-      <Link to="/buscar" className="link">Buscar</Link>
-      <Link to="/sobre-nosotros" className="link">About Us</Link>
-      <Link to="/contacto" className="link">Contact</Link>
-      </div>
+      <div className="navbar">
+        <Link to="/" className="hackflix">
+          HACKFLIX
+        </Link>
+        <div>
+          <Link to="/paginacion/1" className="navbar-link">
+            Ver por Paginas
+          </Link>
+          <Link to="/buscar" className="navbar-link">
+            Buscar
+          </Link>
+          <Link to="/sobre-nosotros" className="navbar-link">
+            About The Project
+          </Link>
+        </div>
       </div>
     </section>
   );
--- src/components/HeaderContent.jsx
+++ src/components/HeaderContent.jsx
@@ -1,11 +1,12 @@
-function HeaderContent ({title, date, content}){
-    return(
-        <section className="header-content">
-            <h1>{title}</h1>
-            <small>{date}</small>
-            <p>{content}</p>
-        </section>
-    )
+function HeaderContent({ title, date, content }) {
+  return (
+    <section className="header-content">
+      <h1>{title}</h1>
+      <small>{date}</small>
+      <p>{content}</p>
+      <p className="scroll">Scroll to continue...</p>
+    </section>
+  );
 }
 
 export default HeaderContent;
