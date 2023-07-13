49c0078a2132ac2ffe419e13cd9056e16333673e
gbascans <gbascansc@gmail.com>
2023-07-13 16:05:29 -0300

    Page not found

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,4 +1,17 @@
-function NoMatch(){
-    return <h1>404 error. P'agina no encontrada</h1>
+import "../css/NoMatch.css";
+import { Link } from "react-router-dom";
+
+function NoMatch() {
+  return (
+    <>
+      <section className="no-match-section"></section>
+      <div className="no-match-container">
+        <h1 className="no-match-alert">Page Not Found</h1>
+        <Link to="/" className="text-decoration-none">
+          BACK TO HOME
+        </Link>
+      </div>
+    </>
+  );
 }
-export default NoMatch
+export default NoMatch;
